871c8d068cb6f16d413d834586a54c1efd454c26
Daniil Blair <30020332@student.toiohomai.ac.nz>
2019-03-13 11:37:23 +1300

    comment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Helo my name is Daniil Blair. I am the head developer of this programme.
+
+            Console.WriteLine("Hello. Welcome to the guess the bird game.");
+            Console.WriteLine("You have three diferent options of birds to pick from.");
+            Console.WriteLine("Your options are, Pigeon, Chicken and Parrot.");
+            Console.WriteLine("If you guess the right bird you will win a billions $");
+
+
+
+
+        }
+    }
+}
