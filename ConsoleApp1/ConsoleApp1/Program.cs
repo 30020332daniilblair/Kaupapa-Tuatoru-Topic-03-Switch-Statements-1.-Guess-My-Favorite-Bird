fe0b824f5fbb0d4bb254951e18650082df563a06
Daniil Blair <30020332@student.toiohomai.ac.nz>
2019-03-13 16:59:36 +1300

    comment

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,13 +12,34 @@ namespace ConsoleApp1
         {
             // Helo my name is Daniil Blair. I am the head developer of this programme.
 
-            Console.WriteLine("Hello. Welcome to the guess the bird game.");
+            Console.WriteLine("Hello. Welcome to guess the bird game.");
             Console.WriteLine("You have three diferent options of birds to pick from.");
             Console.WriteLine("Your options are, Pigeon, Chicken and Parrot.");
-            Console.WriteLine("If you guess the right bird you will win a billions $");
+            Console.WriteLine("If you guess correctly you will win a billion $");
+            Console.WriteLine("Go ahead and select a bird");
 
+            string bird = Console.ReadLine().ToLower();
+                
 
+            switch (bird)
+            {
+                case "pigeon":
+                case "chicken":
+                    Console.WriteLine("Na you did not get it this time sorry");
+                    break;
 
+                case "parrot":
+                    Console.WriteLine("Congratulations! You are now a billionaire");
+                       
+                    break;
+
+                default:
+                    Console.WriteLine("That was an invalid key");
+                    break;
+
+            }
+
+            Console.ReadLine();
 
         }
     }
